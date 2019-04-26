0d75511da48a475dd5f6fc7d5e3d8dc2dfd1a41c
Aziz Ganiev <ajganiev@gmail.com>
2019-04-26 14:56:33 +0500

    relations in an

--- app/Announcement.php
+++ app/Announcement.php
@@ -7,4 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class Announcement extends Model
 {
     //
+    protected $fillable = [
+
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function photos() {
+        return $this->hasMany(Photo::class);
+    }
 }
--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    // belongs to an
+
+    // belongs to user
 }
