d20599ea4db0b3110fad695fa2274d16d0499ccd
Aziz Ganiev <ajganiev@gmail.com>
2019-05-05 17:39:27 +0500

    fixes

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -8,65 +8,88 @@
       <h5 class="card-title">Card title that wraps to a new line</h5>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <p class="card-text"><p class="text-muted">
-      <a href="#">Aziz The Sloth</a> 3 mins ago</p></p>
-      <a href="#" class="btn btn-success float-left">View</a>
-      <div class="float-right" style="margin-bottom: 10px">
-        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_up</i></a>
-        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_down</i></a>
+        <a href="#">Aziz The Sloth</a> 3 mins ago</p>
+        <small class="text-muted"><i class="material-icons small">visibility</i>235 views</small>
+        <small class="text-muted"><i class="material-icons small">comment</i>10 commments</small>
+      </p>
+      <div class="float-left">
+        <a href="#" class="btn btn-success"><i class="material-icons small">assignment</i> View</a>
+       
       </div>
-    </div>
-  </div>
-
-<div class="card">
-    <div class="card-body">
-      <h5 class="card-title">Card title that wraps to a new line</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><p class="text-muted">
-      <a href="#">Aziz The Sloth</a> 3 mins ago</p></p>
-      <a href="#" class="btn btn-success float-left">View</a>
       <div class="float-right" style="margin-bottom: 10px">
         <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_up</i></a>
         <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_down</i></a>
+       
       </div>
     </div>
   </div>
 
   <div class="card">
-   
+    <!-- <img class="card-img-top"src="https://picsum.photos/400" alt="Card image cap"> -->
     <div class="card-body">
       <h5 class="card-title">Card title that wraps to a new line</h5>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <p class="card-text"><p class="text-muted">
-      <a href="#">Aziz The Sloth</a> 3 mins ago</p></p>
-      <a href="#" class="btn btn-success float-left">View</a>
+        <a href="#">Aziz The Sloth</a> 3 mins ago</p>
+        <small class="text-muted"><i class="material-icons small">visibility</i>235 views</small>
+        <small class="text-muted"><i class="material-icons small">comment</i>10 commments</small>
+      </p>
+      <div class="float-left">
+        <a href="#" class="btn btn-success"><i class="material-icons small">assignment</i> View</a>
+       
+      </div>
       <div class="float-right" style="margin-bottom: 10px">
         <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_up</i></a>
         <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_down</i></a>
+       
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <!-- <img class="card-img-top"src="https://picsum.photos/400" alt="Card image cap"> -->
+    <div class="card-body">
+      <h5 class="card-title">Card title that wraps to a new line</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><p class="text-muted">
+        <a href="#">Aziz The Sloth</a> 3 mins ago</p>
+        <small class="text-muted"><i class="material-icons small">visibility</i>235 views</small>
+        <small class="text-muted"><i class="material-icons small">comment</i>10 commments</small>
+      </p>
+      <div class="float-left">
+        <a href="#" class="btn btn-success"><i class="material-icons small">assignment</i> View</a>
+       
+      </div>
+      <div class="float-right" style="margin-bottom: 10px">
+        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_up</i></a>
+        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_down</i></a>
+       
       </div>
     </div>
   </div>
 
 
   <div class="card">
-    <img class="card-img-top"src="https://picsum.photos/400/" alt="Card image cap">
+    <img class="card-img-top"src="https://picsum.photos/400" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Card title that wraps to a new line</h5>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <p class="card-text"><p class="text-muted">
-      <a href="#">Aziz The Sloth</a> 3 mins ago</p></p>
-      <a href="#" class="btn btn-success float-left">View</a>
+        <a href="#">Aziz The Sloth</a> 3 mins ago</p>
+        <small class="text-muted"><i class="material-icons small">visibility</i>235 views</small>
+        <small class="text-muted"><i class="material-icons small">comment</i>10 commments</small>
+      </p>
+      <div class="float-left">
+        <a href="#" class="btn btn-success"><i class="material-icons small">assignment</i> View</a>
+       
+      </div>
       <div class="float-right" style="margin-bottom: 10px">
         <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_up</i></a>
         <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_down</i></a>
+       
       </div>
     </div>
   </div>
-
-
-
-
-
-
 </div>
 
 
