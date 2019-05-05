05bf89f63c9d2bcaa8336785609d74ee5f0978f3
Aziz Ganiev <ajganiev@gmail.com>
2019-05-05 17:14:01 +0500

    pimped up main layout

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -1,62 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
-<!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"-->
 <div class="card-columns">
   <div class="card">
-
-	<div style=" margin: 10px; margin-left: 15px;">
-  		<a href="#to_user_profile" style="font-size: 16px; text-align: bottom;"><img class="card-img-top rounded-circle float-left "  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_DZ6ET1hmhXoNt9wNyvv2IXAzIfoievwbHcKAqKuFcvTiWndcLw" alt="Card image cap" style="width: 50px; height: 50px;"> Abihamid Abdumalikov</a>
-  	</div>
-    <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLSDtdAOHXZCEEL8ScUqAENlq8-72Xnu8xt9zN2TD9T4O7vYGsqw" alt="Card image cap" style="margin-top: 10px;">
+    <img class="card-img-top"src="https://picsum.photos/400" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Card title that wraps to a new line</h5>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><p class="text-muted">
+      <a href="#">Aziz The Sloth</a> 3 mins ago</p></p>
+      <a href="#" class="btn btn-success float-left">View</a>
+      <div class="float-right" style="margin-bottom: 10px">
+        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_up</i></a>
+        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_down</i></a>
+      </div>
     </div>
-    <a href="#" class="btn btn-primary" style="margin: 15px; margin-top: 0px;">More...</a>
-   <button class='btn btn-lg ' style='background-color:transparent;'>
+  </div>
+
+<div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Card title that wraps to a new line</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><p class="text-muted">
+      <a href="#">Aziz The Sloth</a> 3 mins ago</p></p>
+      <a href="#" class="btn btn-success float-left">View</a>
+      <div class="float-right" style="margin-bottom: 10px">
+        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_up</i></a>
+        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_down</i></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
    
-
-
-   	<i class="material-icons md-18" style="margin: 10px" >thumb_up</i>
-    <i class="material-icons md-18" style="margin: 10px" >thumb_down</i>
-
-   	
-	
+    <div class="card-body">
+      <h5 class="card-title">Card title that wraps to a new line</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><p class="text-muted">
+      <a href="#">Aziz The Sloth</a> 3 mins ago</p></p>
+      <a href="#" class="btn btn-success float-left">View</a>
+      <div class="float-right" style="margin-bottom: 10px">
+        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_up</i></a>
+        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_down</i></a>
+      </div>
+    </div>
   </div>
-
-
-
-<!--   no photo   -->
-
-
-  <div class="card p-3">
-  	<div style=" margin: 10px; margin-left: 15px;">
-  		<a href="#to_user_profile" style="font-size: 16px; text-align: bottom;"><img class="card-img-top rounded-circle float-left "  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_DZ6ET1hmhXoNt9wNyvv2IXAzIfoievwbHcKAqKuFcvTiWndcLw" alt="Card image cap" style="width: 50px; height: 50px;">  Abihamid Abdumalikov</a>
-  	</div>
-    <blockquote class="blockquote mb-0 card-body">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-      <footer class="blockquote-footer">
-        <small class="text-muted">
-          Someone famous in <cite title="Source Title">Source Title</cite>
-        </small>
-      </footer>
-    </blockquote>
-  </div>
-
-
-
-
 
 
   <div class="card">
-    <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_kzJ0rydH34OAftFVHOlKNkEywRfLw3qWgb_xRBk3GkDxq4CU" alt="Card image cap">
+    <img class="card-img-top"src="https://picsum.photos/400/" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <h5 class="card-title">Card title that wraps to a new line</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><p class="text-muted">
+      <a href="#">Aziz The Sloth</a> 3 mins ago</p></p>
+      <a href="#" class="btn btn-success float-left">View</a>
+      <div class="float-right" style="margin-bottom: 10px">
+        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_up</i></a>
+        <a href="#"><i class="material-icons medium" style="margin: 10px" >thumb_down</i></a>
+      </div>
     </div>
   </div>
+
+
+
+
+
 
 </div>
 
