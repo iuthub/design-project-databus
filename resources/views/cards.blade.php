8b01b176a9f7e09e170d1b47bbfe9113c5a2c961
Ruslan Musyaev <musyaev@gmail.com>
2019-05-05 13:17:42 +0500

    added new card page

---
+++ resources/views/cards.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row">
+  <div class="col">
+				  	<div class="card" style="width: 300px; margin: 0px; padding: 10px">
+					  		
+				  	<div style="margin-bottom: 5px">
+				  		<img class="card-img-top rounded-circle float-left" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_DZ6ET1hmhXoNt9wNyvv2IXAzIfoievwbHcKAqKuFcvTiWndcLw" alt="Card image cap" style="width: 40px; height: 40px;">
+				  		<p class="float-bottom" style="font-size: 10px"><a href="#to_user_profile" style="font-size: 16px">__Username__</a> from Mirobod Tumani</p>
+				  	</div>
+
+
+
+
+
+
+
+
+					  	<img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLSDtdAOHXZCEEL8ScUqAENlq8-72Xnu8xt9zN2TD9T4O7vYGsqw" alt="Card image cap">
+					  	<div class="card-body">
+					    	<p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					    	<a href="#" class="btn btn-primary">Go somewhere</a>
+					  	</div>
+					</div>
+  	</div>
+
+  	<div class="col">
+
+  					<div class="card" style="width: 300px; margin: 0px;">
+					  	<img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_kzJ0rydH34OAftFVHOlKNkEywRfLw3qWgb_xRBk3GkDxq4CU" alt="Card image cap">
+					  	<div class="card-body">
+					    	<p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					  	</div>
+					</div>
+
+  	</div>
+
+  	<div class="col">
+
+  					<div class="card" style="width: 300px; margin: 0px;">
+					  	<img class="card-img-top" src="https://resizing.flixster.com/RTEPRtAo8V2i6rhWstiQiQdahDc=/300x300/v1.bjsxOTM1NDIyO2o7MTgwNjc7MTIwMDs2NjA7OTAw" alt="Card image cap">
+					  	<div class="card-body">
+					    	<p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					  	</div>
+					</div>
+
+  	</div>
+
+  	<div class="col">
+				  	<div class="card" style="width: 300px; margin: 0px;">
+					  	<img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfqVr3KmXRZZhltFafusphPxx_G3AslSzOBZgkXBj4BRZ_QvbB" alt="Card image cap">
+					  	<div class="card-body">
+					    	<p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					  	</div>
+					</div>
+  	</div>
+
+
+  	<div class="w-100"></div>
+  	<div class="col">col</div>
+  	<div class="col">col</div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/cards', function () {
+    return view('cards');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
